5527195c0779accfcddf945a2a2c5b6473db245f
ludwig.gaertner <ludwig.gaertner@taxdoo.com>
2025-12-29 21:39:53 +0100

    working

--- bin/external/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ludwig.gaertner/Documents/Code/private/cpendulum/external
-# Build directory: /Users/ludwig.gaertner/Documents/Code/private/cpendulum/bin/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
