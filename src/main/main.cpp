7ff30157b305a94f41de53f357ec114650db7613
ludwig.gaertner <ludwig.gaertner@taxdoo.com>
2026-01-07 14:47:56 +0100

    working

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,5 +1,6 @@
 #include "Pendulum.hpp"
 #include <ncurses.h>
+#include <ratio>
 #include <thread>
 #include <chrono>
 #include <string>
@@ -38,28 +39,28 @@ void draw_frame() {
     mvprintw(LINES - 1, 0, "Press any key to exit ...");
 }
 
-void draw_pendulum(Pendulum pendulum, int horizontal_off_center) {
+void draw_pendulum(Pendulum pendulum, double relative_horizontal_position) {
     auto pivot = pendulum.getPivot();
 
     auto max_height = pivot.y * 2;
     auto max_width_from_center = pivot.y;
 
+    int x_offset = COLS * relative_horizontal_position;
+
     const auto base_line = LINES - 2;
 
     int pivot_y = base_line - (pivot.y / max_height) * LINES;
-    int pivot_x = (pivot.x / max_width_from_center) * COLS / 2 + COLS / 2 + horizontal_off_center;
+    int pivot_x = (pivot.x / max_width_from_center) * COLS / 2 + x_offset; 
 
     for (int a = pivot_y+1; a < LINES - 2; a ++)
         mvaddch(a, pivot_x, '|');
 
     auto weight1 = pendulum.getWeight1();
     int weight_1_y = base_line - (weight1.y / max_height) * LINES;
-    int weight_1_x = (weight1.x / max_width_from_center) * COLS * 0.6 / 2 + COLS / 2 + horizontal_off_center;
-
+    int weight_1_x = (weight1.x / max_width_from_center) * COLS * 0.6 / 2 + x_offset;
     auto weight2 = pendulum.getWeight2();
     int weight_2_y = base_line - (weight2.y / max_height) * LINES;
-    int weight_2_x = (weight2.x / max_width_from_center) * COLS * 0.6 / 2 + COLS / 2 + horizontal_off_center;
-
+    int weight_2_x = (weight2.x / max_width_from_center) * COLS * 0.6 / 2 + x_offset;
     draw_line(weight_1_x, weight_1_y, weight_2_x, weight_2_y);
     draw_line(weight_1_x, weight_1_y, pivot_x, pivot_y);
 
@@ -68,13 +69,18 @@ void draw_pendulum(Pendulum pendulum, int horizontal_off_center) {
     mvaddch(weight_2_y, weight_2_x, 'O');
 }
 
-void print_energy(Pendulum pendulum) {
+void print_energy(Pendulum pendulum, int position) {
 
     std::string l1 = std::format("Pendulum total energy: {}", std::round(pendulum.energy().total() * 100.0) / 100.0).c_str();
     std::string l2 = std::format("E kinetic: {}", std::round(pendulum.energy().kin * 100.0) / 100.0).c_str();
     std::string l3 = std::format("E potential: {}", std::round(pendulum.energy().pot * 100.0) / 100.0).c_str();
 
-    int pos_x = COLS - 30;
+    int pos_x;
+    if (position == 1) {
+        pos_x = COLS - 30;
+    } else {
+        pos_x = 1;
+    }
 
     mvprintw(1, pos_x, "%s", l1.c_str());
     mvprintw(2, pos_x, "%s", l2.c_str());
@@ -94,36 +100,37 @@ void draw(Pendulum pendulum_1, Pendulum pendulum_2) {
     // If no input is ready, getch returns ERR.
     nodelay(stdscr, TRUE);
 
+    int h_ms = 1;
+
     bool run = true;
     for (int a = 0; run; a++) {
-        // 2. Draw the character
-        // move(y, x) then addch(char)
-        // Note: ncurses uses (y, x) order, not (x, y)
-
+        auto start = std::chrono::high_resolution_clock::now();
         if (a % 30 == 0) {
             clear();
             draw_frame();
-            draw_pendulum(pendulum_1, 30);
-            draw_pendulum(pendulum_2, -30);
-            print_energy(pendulum_1);
-
-
-            // 3. Refresh the screen to show the changes
+            draw_pendulum(pendulum_1, 1/3.0);
+            draw_pendulum(pendulum_2, 2/3.0);
+            // print_energy(pendulum_1, 1);
+            // print_energy(pendulum_2, 0);
             refresh();
+
+            a = 0;
         }
 
         // 4. Wait for user input so the program doesn't close instantly
         if (getch() != ERR) run = false;
 
         if (false) {
-            pendulum_1.calculateAndApplyForcesEulerCromer(1);
-            pendulum_2.calculateAndApplyForcesEulerCromer(1);
+            pendulum_1.calculateAndApplyForcesEulerCromer(h_ms);
+            pendulum_2.calculateAndApplyForcesEulerCromer(h_ms);
         } else {
-            pendulum_1.calculateAndApplyForcesRungeKutta(1);
-            pendulum_2.calculateAndApplyForcesRungeKutta(1);
+            pendulum_1.calculateAndApplyForcesRungeKutta(h_ms);
+            pendulum_2.calculateAndApplyForcesRungeKutta(h_ms);
         }
-        auto loop_duration = std::chrono::milliseconds(1);
-        std::this_thread::sleep_for(loop_duration);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto sleep_duration = std::chrono::duration<int, std::milli>(h_ms) - (end - start);
+        std::this_thread::sleep_for(sleep_duration);
 
     }
 
